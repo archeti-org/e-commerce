f766dff884471c224c733249dbaee2cbb7da1ae7
Carmen Rondon Regalado <crondon@archeti.com>
2021-04-22 12:31:10 -0400

    [MIG] website_sale_secondary_unit : Migration to 14.0
    Update description
    run pre-commit
    add oca dependency
    Conflicts:
    	website_sale_secondary_unit/static/src/js/website_sale_secondary_unit.js

--- website_sale_secondary_unit/static/src/js/website_sale_secondary_unit.js
+++ website_sale_secondary_unit/static/src/js/website_sale_secondary_unit.js
@@ -1,13 +1,35 @@
-odoo.define("website_sale_secondary_unit.animation", function(require) {
+odoo.define("website_sale_secondary_unit.animation", function (require) {
     "use strict";
 
     const VariantMixin = require("sale.VariantMixin");
     const sAnimation = require("website.content.snippets.animation");
+    var publicWidget = require("web.public.widget");
+    require("website_sale.website_sale");
+
+    publicWidget.registry.WebsiteSale.include({
+        _submitForm: function () {
+            if (
+                this.$form.find('input[name="add_secondary_qty"]').val() !== undefined
+            ) {
+                this.rootProduct.add_secondary_qty = parseFloat(
+                    this.$form.find('input[name="add_secondary_qty"]').val()
+                );
+            }
+            if (
+                this.$form.find('select[name="secondary_uom_id"]').val() !== undefined
+            ) {
+                this.rootProduct.secondary_uom_id = parseFloat(
+                    this.$form.find('select[name="secondary_uom_id"]').val()
+                );
+            }
+            return this._super();
+        },
+    });
 
     sAnimation.registry.sale_secondary_unit = sAnimation.Class.extend(VariantMixin, {
         selector: ".secondary-unit",
         // eslint-disable-next-line no-unused-vars
-        init: function(parent, editableMode) {
+        init: function (parent, editableMode) {
             this._super.apply(this, arguments);
             this.$secondary_uom = null;
             this.$secondary_uom_qty = null;
@@ -38,7 +60,7 @@ odoo.define("website_sale_secondary_unit.animation", function(require) {
                 _this._onChangeSecondaryUom();
             });
         },
-        _setValues: function() {
+        _setValues: function () {
             this.secondary_uom_qty = Number(
                 this.$target.find(".secondary-quantity").val()
             );
@@ -63,7 +85,7 @@ odoo.define("website_sale_secondary_unit.animation", function(require) {
             this.$product_qty.val(this.secondary_uom_qty * factor);
             this.onChangeAddQuantity(ev);
         },
-        _onChangeProductQty: function() {
+        _onChangeProductQty: function () {
             this._setValues();
             const factor = this.secondary_uom_factor * this.product_uom_factor;
             this.$secondary_uom_qty.val(this.product_qty / factor);
@@ -73,7 +95,7 @@ odoo.define("website_sale_secondary_unit.animation", function(require) {
     sAnimation.registry.sale_secondary_unit_cart = sAnimation.Class.extend({
         selector: ".oe_cart",
         // eslint-disable-next-line no-unused-vars
-        init: function(parent, editableMode) {
+        init: function (parent, editableMode) {
             this._super.apply(this, arguments);
             this.$product_qty = null;
             this.secondary_uom_qty = null;
@@ -81,17 +103,17 @@ odoo.define("website_sale_secondary_unit.animation", function(require) {
             this.product_uom_factor = null;
             this.product_qty = null;
         },
-        start: function() {
+        start: function () {
             var _this = this;
             this.$target.on(
                 "change",
                 "input.js_secondary_quantity[data-line-id]",
-                function() {
+                function () {
                     _this._onChangeSecondaryUom(this);
                 }
             );
         },
-        _setValues: function(order_line) {
+        _setValues: function (order_line) {
             this.$product_qty = this.$target.find(
                 ".quantity[data-line-id=" + order_line.dataset.lineId + "]"
             );
@@ -99,7 +121,7 @@ odoo.define("website_sale_secondary_unit.animation", function(require) {
             this.secondary_uom_factor = Number(order_line.dataset.secondaryUomFactor);
             this.product_uom_factor = Number(order_line.dataset.productUomFactor);
         },
-        _onChangeSecondaryUom: function(order_line) {
+        _onChangeSecondaryUom: function (order_line) {
             this._setValues(order_line);
             const factor = this.secondary_uom_factor * this.product_uom_factor;
             this.$product_qty.val(this.secondary_uom_qty * factor);
